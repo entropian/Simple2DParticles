eaa226bd8f592a6525f438618e6385ed4e1f7c99
Bowen Yan <KH2010G@gmail.com>
2018-12-04 20:01:27 -0600

    Fixing last commit

---
+++ platform.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifdef _WIN32
+#include <Windows.h>
+
+// Sets affinity to CPU 0
+bool setAffinity()
+{
+	HANDLE proc_handle = GetCurrentProcess();
+	DWORD_PTR proc_affinity_mask = 1;
+	BOOL success = SetProcessAffinityMask(proc_handle, proc_affinity_mask);
+	SetPriorityClass(proc_handle, THREAD_PRIORITY_TIME_CRITICAL);
+	return success;
+}
+
+#endif
